75b934eef7892deadf1c1fd5bb28a9c44e03a5d3
iampeterbanjo <peterbanjo@gmail.com>
2019-01-05 10:58:53 +0000

    use scss for styles

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
-const Path = require('path');
+const path = require('path');
 const autoprefixer = require('autoprefixer');
 const gulp = require('gulp');
+const less = require('gulp-less');
 const precss = require('precss');
+const sass = require('gulp-sass');
 
-const tachyonsPath = require.resolve('tachyons');
-const stylesBuildPath = Path.join(__dirname, './public/build/assets/styles');
-const stylesSrcPath = Path.join(__dirname, './assets/styles');
-const cssSrcPath = `${stylesSrcPath}/*.css`;
+sass.compiler = require('node-sass');
+
+const stylesBuildPath = path.join(__dirname, './public/build/assets/styles');
+const stylesSrcPath = path.join(__dirname, './assets/styles');
+const cssSrcPath = `${stylesSrcPath}/*.{css,scss}`;
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 
 const buildCss = () => {
-  const postcss = require('gulp-postcss');
-  const sourcemaps = require('gulp-sourcemaps');
   return gulp
-    .src([tachyonsPath, cssSrcPath])
+    .src([cssSrcPath])
+    .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.init())
     .pipe(postcss([precss, autoprefixer]))
     .pipe(sourcemaps.write('.'))
@@ -22,5 +26,5 @@ const buildCss = () => {
 gulp.task('css', buildCss);
 
 gulp.task('css-watch', () => {
-  gulp.watch(`${stylesSrcPath}/*.css`, buildCss);
+  gulp.watch(`${stylesSrcPath}/*.{css,scss}`, buildCss);
 });
