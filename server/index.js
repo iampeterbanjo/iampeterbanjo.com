4dee09a2a2e628c1ce279a49b975dc738f6d5568
iampeterbanjo <peterbanjo@gmail.com>
2019-02-01 21:43:36 +0000

    add caddy config

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const Hapi = require('hapi');
-const path = require('path');
 const Inert = require('inert');
+
+const path = require('path');
 const cssPath = path.join(__dirname, '../public/build/assets/styles/');
 const imagePath = path.join(__dirname, '../public/build/assets/images/');
 const staticPath = path.join(__dirname, '../public/build/static/');
@@ -9,7 +10,7 @@ const blogPath = path.join(__dirname, '../public/build/static/blog/');
 (async () => {
   const server = Hapi.server({
     host: '0.0.0.0',
-    port: Number(process.env.PORT || 8080),
+    port: Number(process.env.API_PORT || 8080),
     routes: {
       files: {
         relativeTo: __dirname
@@ -17,63 +18,11 @@ const blogPath = path.join(__dirname, '../public/build/static/blog/');
     }
   });
 
-  const Statics = {
-    name: 'serve-static-files',
-    version: '0.0.1',
-    register: (server, { blogPath, cssPath, staticPath }) => {
-      server.route({
-        path: '/{path*}',
-        method: 'GET',
-        handler: {
-          directory: {
-            path: staticPath,
-            listing: false,
-            index: true
-          }
-        }
-      });
-
-      server.route({
-        path: '/css/{path*}',
-        method: 'GET',
-        handler: {
-          directory: {
-            path: cssPath,
-            listing: false,
-            index: false
-          }
-        }
-      });
-
-      server.route({
-        path: '/images/{path*}',
-        method: 'GET',
-        handler: {
-          directory: {
-            path: imagePath,
-            listing: false,
-            index: false
-          }
-        }
-      });
-
-      server.route({
-        path: '/blog/{path*}',
-        method: 'GET',
-        handler: {
-          directory: {
-            path: blogPath,
-            listing: false,
-            index: true
-          }
-        }
-      });
-    }
-  };
+  const Statics = require('./statics');
 
   try {
     await server.register(Inert);
-    await server.register({ plugin: Statics, options: { blogPath, cssPath, staticPath }});
+    await server.register({ plugin: Statics, options: { blogPath, cssPath, staticPath, imagePath }});
     await server.start();
 
     console.log(`Server running at: ${server.info.uri}`);
