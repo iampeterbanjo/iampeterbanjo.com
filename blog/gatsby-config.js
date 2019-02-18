720d68d85f2c9a736b22d170e1fb84230255b244
iampeterbanjo <peterbanjo@gmail.com>
2019-02-18 06:21:22 +0000

    refactored plugins into plugins/

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,4 +1,7 @@
-const { blog, assets } = require('./plugins/gatsby-source-filesystem')
+const { blog, assets } = require('./plugins/gatsby-source-filesystem');
+const remark = require('./plugins/gatsby-transformer-remark');
+const manifest = require('./plugins/gatsby-plugin-manifest');
+const typography = require('./plugins/gatsby-plugin-typography');
 
 module.exports = {
   siteMetadata: {
@@ -7,62 +10,25 @@ module.exports = {
     description: `A starter blog demonstrating what Gatsby can do.`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
-    },
+      twitter: `kylemathews`
+    }
   },
   plugins: [
     blog,
     assets,
-    {
-      resolve: `gatsby-transformer-remark`,
-      options: {
-        plugins: [
-          {
-            resolve: `gatsby-remark-images`,
-            options: {
-              maxWidth: 590,
-            },
-          },
-          {
-            resolve: `gatsby-remark-responsive-iframe`,
-            options: {
-              wrapperStyle: `margin-bottom: 1.0725rem`,
-            },
-          },
-          `gatsby-remark-prismjs`,
-          `gatsby-remark-copy-linked-files`,
-          `gatsby-remark-smartypants`,
-        ],
-      },
-    },
+    remark,
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
+      }
     },
     `gatsby-plugin-feed`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `Gatsby Starter Blog`,
-        short_name: `GatsbyJS`,
-        start_url: `/`,
-        background_color: `#ffffff`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `content/assets/gatsby-icon.png`,
-      },
-    },
+    manifest,
     `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
-  ],
-}
+    typography
+  ]
+};
---
+++ blog/plugins/gatsby-plugin-manifest.js
@@ -0,0 +1,12 @@
+module.exports = {
+  resolve: `gatsby-plugin-manifest`,
+  options: {
+    name: `Gatsby Starter Blog`,
+    short_name: `GatsbyJS`,
+    start_url: `/`,
+    background_color: `#ffffff`,
+    theme_color: `#663399`,
+    display: `minimal-ui`,
+    icon: `content/assets/gatsby-icon.png`
+  }
+};
