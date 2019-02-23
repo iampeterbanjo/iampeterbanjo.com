ac8d83f7239322a2beb4d5d9918de7c1b3cd2cc1
iampeterbanjo <peterbanjo@gmail.com>
2019-02-23 23:00:05 +0000

    edit content. make links obvious

--- blog/content/blog/omg-test-faves/index.md
+++ blog/content/blog/omg-test-faves/index.md
@@ -31,6 +31,7 @@ describe('todos reducer', () => {
   });
 });
 ```
+[Figure 1: Redux reducer test from redux documentation](https://redux.js.org/recipes/writing-tests#example-2)
 
 At first glance this looks like a unit test but it also tests the reducer types. And when I want to expand the store I can easily use a test-driven approach by using [expect.objectContaining](https://jestjs.io/docs/en/expect#expectobjectcontainingobject) instead of `toEqual`.
 
@@ -47,6 +48,7 @@ describe('GET /user', function() {
   });
 });
 ```
+[Figure 2: Express integration test from Supertest documentation](https://github.com/visionmedia/supertest)
 
 These integration tests run quickly and make it possible to test routes, controllers and models all in one go. Although Express is the #1 NodeJS server framework, I [prefer HapiJS](https://hueniverse.com/why-you-should-consider-hapi-6163689bd7c2) but more on that in another post.
 
@@ -64,5 +66,6 @@ test('should fetch users', () => {
   return Users.all().then(resp => expect(resp.data).toEqual(users));
 });
 ```
+[Figure 3: Mock of axios from documentation Jest documentation](https://jestjs.io/docs/en/mock-functions#mocking-modules)
 
 This little bit of wizadry turns slow integration tests into faster unit tests by removing the need for an API to respond. Of course mocks can be used for many things but **this** is [my number #1 guy](https://www.youtube.com/watch?v=-VRwTiQWoXU).
--- blog/src/components/footer.js
+++ blog/src/components/footer.js
@@ -11,19 +11,19 @@ const content = data => {
   const { author, social } = data.site.siteMetadata;
 
   return (
-    <footer class="bg-white black-70 ph3 ph3-ns pv3 pv5-ns bt b--black-10">
-      <div class="mw9 center">
-        <div class="lh-copy">
+    <footer className="bg-white black-70 ph3 ph3-ns pv3 pv5-ns bt b--black-10">
+      <div className="mw9 center">
+        <div>
           <Nav />
         </div>
-        <div class="mt4 mb4">
+        <div className="mt4 mb4">
           <a
-            class="ba br2 black-70 no-underline grow b inline-flex items-center mr3 pv2 ph3"
+            className="btn ba br2 black-70 no-underline grow b inline-flex items-center mr3 pv2 ph3"
             href={`https://github.com/${social.github}`}
             title={`${author} on GitHub`}
           >
             <div
-              class="w2 pv1 pr2"
+              className="w2 pv1 pr2"
               dangerouslySetInnerHTML={{
                 __html: feather.icons.github.toSvg({ class: 'github svg-icon' })
               }}
@@ -32,12 +32,12 @@ const content = data => {
             <span>GitHub</span>
           </a>
           <a
-            class="ba br2 black-70 no-underline grow b inline-flex items-center mr3 pv2 ph3"
+            className="btn ba br2 black-70 no-underline grow b inline-flex items-center mr3 pv2 ph3"
             href={`https://github.com/${social.twitter}`}
             title={`${author} on Twitter`}
           >
             <div
-              class="w2 pv1 pr2"
+              className="w2 pv1 pr2"
               dangerouslySetInnerHTML={{
                 __html: feather.icons.twitter.toSvg({
                   class: 'twitter svg-icon'
@@ -48,7 +48,7 @@ const content = data => {
             <span>Twitter</span>
           </a>
         </div>
-        <p class="f6 measure copy lh-copy">
+        <p className="f6 measure copy lh-copy">
           © {new Date().getFullYear()}, Made with
           {` `}
           sleep
