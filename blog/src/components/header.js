a077d8162947d1a19547c205180a6ecc5db18835
iampeterbanjo <peterbanjo@gmail.com>
2019-02-25 05:19:28 +0000

    hide home link on small screens

--- blog/src/components/header.js
+++ blog/src/components/header.js
@@ -4,12 +4,12 @@ export default function Header(props) {
   return (
     <header class="w-100 pa3 pa5-ns bg-white mb4">
       <div class="dib dt-ns mw9 center w-100">
-        <div class="dib flex justify-center tl w-50-ns w-25">
+        <div class="dn dib-ns flex justify-center tl w-50-ns w-25">
           <a href="/" class="f5 f4-ns fw6 mt0 mb1 code" title="Home">
             iam
           </a>
         </div>
-        <div class="dib tl w-50-ns w-75">{props.children}</div>
+        <div class="dib tl w-50-ns w-100">{props.children}</div>
       </div>
     </header>
   );
