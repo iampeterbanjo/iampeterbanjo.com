fe08403b800b64f7ac4f155853296d88878616d2
iampeterbanjo <peterbanjo@gmail.com>
2019-02-26 05:42:17 +0000

    add colour theme

--- blog/src/components/nav.js
+++ blog/src/components/nav.js
@@ -12,15 +12,15 @@ export default function Nav(props) {
       role="navigation"
       aria-label="main navigation"
     >
-      <a className="f6 fw6 black-70 mr2 mr3-m mr4-l dib" href="/">
+      <a className="f6 fw6 mr2 mr3-m mr4-l dib" href="/">
         Home
       </a>
-      <a className="f6 fw6 black-70 mr2 mr3-m mr4-l dib" href="/list">
+      <a className="f6 fw6 mr2 mr3-m mr4-l dib" href="/list">
         Blog
       </a>
-      <span className="f6 fw6 black-70 mr2 mr3-m mr4-l dib">Projects</span>
-      <span className="f6 fw6 black-70 mr2 mr3-m mr4-l dib">About</span>
-      <span className="f6 fw6 black-70 mr2 mr3-m mr4-l dib">Credits</span>
+      <span className="f6 fw6 mr2 mr3-m mr4-l dib">Projects</span>
+      <span className="f6 fw6 mr2 mr3-m mr4-l dib">About</span>
+      <span className="f6 fw6 mr2 mr3-m mr4-l dib">Credits</span>
     </nav>
   );
 }
