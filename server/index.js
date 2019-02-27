3f345b3af8dd9de0c88c442cc45621e4e7ab967e
iampeterbanjo <peterbanjo@gmail.com>
2019-02-27 06:04:20 +0000

    create server module for testing

--- server/index.js
+++ server/index.js
@@ -1,57 +1,16 @@
-const Hapi = require('hapi');
-const Inert = require('inert');
+const Server = require('./server');
 
-const path = require('path');
-const cssPath = path.join(__dirname, '../public/build/assets/styles/');
-const imagePath = path.join(__dirname, '../public/build/assets/images/');
-const staticPath = path.join(__dirname, '../blog/public/');
-const blogPath = path.join(__dirname, '../blog/public/');
-
-(async () => {
-  const server = Hapi.server({
-    host: '0.0.0.0',
-    port: Number(process.env.PORT || 8080),
-    routes: {
-      files: {
-        relativeTo: __dirname
-      }
-    }
-  });
+Server(async (err, server) => {
+  if (err) {
+    throw err;
+  }
 
   try {
-    await server.register(Inert);
-    await server.register({
-      plugin: require('./statics'),
-      options: { blogPath, cssPath, staticPath, imagePath }
-    });
-    await server.register({
-      plugin: require('./cqc')
-    });
-    await server.register({
-      plugin: require('./hapi-require-https')
-    });
-    await server.register({
-      plugin: require('good'),
-      options: {
-        reporters: {
-          myConsoleReporter: [
-            {
-              module: 'good-squeeze',
-              name: 'Squeeze',
-              args: [{ log: '*', response: '*' }]
-            },
-            {
-              module: 'good-console'
-            },
-            'stdout'
-          ]
-        }
-      }
-    });
     await server.start();
 
     console.log(`Server running at: ${server.info.uri}`);
   } catch (error) {
     console.warn(error);
+    throw error;
   }
-})();
+});
---
+++ server/server.js
@@ -0,0 +1,59 @@
+const Hapi = require('hapi');
+const Inert = require('inert');
+
+const path = require('path');
+const cssPath = path.join(__dirname, '../public/build/assets/styles/');
+const imagePath = path.join(__dirname, '../public/build/assets/images/');
+const staticPath = path.join(__dirname, '../blog/public/');
+const blogPath = path.join(__dirname, '../blog/public/');
+
+console.log(staticPath);
+
+module.exports = async callback => {
+  const server = Hapi.server({
+    host: '0.0.0.0',
+    port: Number(process.env.PORT || 8080),
+    routes: {
+      files: {
+        relativeTo: __dirname
+      }
+    }
+  });
+
+  try {
+    await server.register(Inert);
+    await server.register({
+      plugin: require('./statics'),
+      options: { blogPath, cssPath, staticPath, imagePath }
+    });
+    await server.register({
+      plugin: require('./cqc')
+    });
+    await server.register({
+      plugin: require('./hapi-require-https')
+    });
+    await server.register({
+      plugin: require('good'),
+      options: {
+        reporters: {
+          myConsoleReporter: [
+            {
+              module: 'good-squeeze',
+              name: 'Squeeze',
+              args: [{ log: '*', response: '*' }]
+            },
+            {
+              module: 'good-console'
+            },
+            'stdout'
+          ]
+        }
+      }
+    });
+
+    callback(null, server);
+  } catch (error) {
+    console.warn(error);
+    callback(error, server);
+  }
+};
