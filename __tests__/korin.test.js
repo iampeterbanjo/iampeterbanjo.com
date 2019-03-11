0227cf3f96be67ed2cf714137e02e4a0cd2e867c
iampeterbanjo <peterbanjo@gmail.com>
2019-03-11 06:14:00 +0000

    create korin api plugin

---
+++ __tests__/korin.test.js
@@ -0,0 +1,26 @@
+const Hapi = require('hapi');
+const { expect } = require('code');
+const { test, before } = (exports.lab = require('lab').script());
+const sinon = require('sinon');
+const got = require('got');
+
+let server = new Hapi.Server();
+
+before(async ({ context }) => {
+  const client = got.extend({ baseUrl: '/' });
+  context.response = { body: 'Korin' };
+  sinon.stub(client, 'get').resolves(context.response);
+
+  server.register({
+    plugin: require('../server/korin'),
+    options: { client }
+  });
+});
+
+test('korin api request returns expected response', async ({ context }) => {
+  const { result } = await server.inject({
+    method: 'GET',
+    url: '/korin'
+  });
+  expect(result).to.equal(context.response.body);
+});
---
+++ server/korin/index.js
@@ -0,0 +1,8 @@
+const got = require('got');
+const { LYRICS_SEARCH_KEY, LYRICS_API_URL } = process.env;
+const client = got.extend({ LYRICS_API_URL, LYRICS_SEARCH_KEY });
+
+module.exports = {
+  plugin: require('./api'),
+  options: { client }
+};
