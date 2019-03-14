c1b2827fef36c11dff15c03f02926bfae762a43f
iampeterbanjo <peterbanjo@gmail.com>
2019-03-14 06:55:24 +0000

    refactoring

--- server/korin/api.js
+++ server/korin/api.js
@@ -1,22 +1,35 @@
-const got = require('got');
 const jsonata = require('jsonata');
+
+const getLyrics = async ({ client, lyricist }, callback) => {
+	try {
+		const query = new URLSearchParams([['q', 'Humble Kendric Lamar']]);
+		const data = (await client.get('/search', { query })).body;
+		const expression = jsonata('response.hits[0].result.id');
+		const songId = expression.evaluate(JSON.parse(data));
+		const { lyrics } = await lyricist.song(songId, { fetchLyrics: true });
+
+		callback(null, lyrics);
+	} catch (error) {
+		callback(error);
+	}
+};
 
 module.exports = {
 	name: 'korin-api',
 	version: '0.0.1',
 	register: (server, { client, lyricist }) => {
+		server.method('getLyrics', getLyrics);
+
 		server.route({
 			path: '/korin',
 			method: 'GET',
 			handler: async (request, reply) => {
 				try {
-					const query = new URLSearchParams([['q', 'Humble Kendric Lamar']]);
-					const data = (await client.get('/search', { query })).body;
-					const expression = jsonata('response.hits[0].result.id');
-					const songId = expression.evaluate(JSON.parse(data));
-					const { lyrics } = await lyricist.song(songId, { fetchLyrics: true });
+					server.method.getLyrics({ client, lyricist }, (error, lyrics) => {
+						if (error) throw error;
 
-					return lyrics || '';
+						reply(lyrics);
+					});
 				} catch (error) {
 					console.warn(error);
 				}
