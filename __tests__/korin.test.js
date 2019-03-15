c9e1cedb492e42f881d73dc498f8adc08fafb3a7
iampeterbanjo <peterbanjo@gmail.com>
2019-03-15 10:53:20 +0000

    add tests for song search query

--- __tests__/korin.test.js
+++ __tests__/korin.test.js
@@ -1,27 +1,80 @@
 const Hapi = require('hapi');
 const { expect } = require('code');
-const { test, before } = (exports.lab = require('lab').script());
+const {
+	test,
+	after,
+	before,
+	describe,
+} = (exports.lab = require('lab').script());
 const sinon = require('sinon');
 const got = require('got');
-const lyricist = require('lyricist');
+const { getLyrics } = require('../server/korin/methods');
+const Lyricist = require('lyricist');
 
 let server = new Hapi.Server();
 
-before(async ({ context }) => {
-	context.lyrics = 'We wish you a merry christmas';
-	const client = got.extend({ baseUrl: '/' });
-	const getLyrics = sinon.stub().resolves(context.lyrics);
+describe('api', () => {
+	before(async ({ context }) => {
+		context.lyrics = 'We wish you a merry christmas';
+		const mockGetLyrics = sinon.stub().resolves(context.lyrics);
 
-	await server.register({
-		plugin: require('../server/korin/api'),
-		options: { client, getLyrics },
+		await server.register({
+			plugin: require('../server/korin/api'),
+			options: { getLyrics: mockGetLyrics },
+		});
+	});
+
+	test('korin api request returns expected response', async ({ context }) => {
+		const { result } = await server.inject({
+			method: 'GET',
+			url: '/korin',
+		});
+		expect(result).to.equal(context.lyrics);
 	});
 });
 
-test('korin api request returns expected response', async ({ context }) => {
-	const { result } = await server.inject({
-		method: 'GET',
-		url: '/korin',
+describe('methods', () => {
+	const client = got.extend({ baseUrl: '/' });
+	const lyricist = new Lyricist(`FAKE-TOKEN`);
+
+	before(({ context }) => {
+		const id = 44444444;
+		const data = {
+			response: {
+				hits: [
+					{
+						result: {
+							id,
+						},
+					},
+				],
+			},
+		};
+
+		context.lyrics = 'Here comes the hot stepper';
+		sinon.stub(client, 'get').resolves({ body: JSON.stringify(data) });
+		sinon.stub(lyricist, 'song').resolves({ lyrics: context.lyrics });
 	});
-	expect(result).to.equal(context.lyrics);
+
+	after(() => {
+		sinon.restore();
+	});
+
+	test('getLyrics returns expected lyrics', async ({ context }) => {
+		const result = await getLyrics({ client, lyricist });
+
+		expect(result).to.equal(context.lyrics);
+	});
+
+	test('client is called with search query', async () => {
+		const term = 'Jump Kriss kross';
+		const query = new URLSearchParams([['q', term]]);
+
+		await getLyrics({ client, lyricist }, term);
+
+		const [first, second] = client.get.args[0];
+
+		expect(first).to.be.equal('/search');
+		expect(second).to.be.equal({ query });
+	});
 });
--- server/korin/methods.js
+++ server/korin/methods.js
@@ -1,15 +1,11 @@
 const jsonata = require('jsonata');
 
 exports.getLyrics = async ({ client, lyricist }, callback) => {
-	try {
-		const query = new URLSearchParams([['q', 'Humble Kendric Lamar']]);
-		const data = (await client.get('/search', { query })).body;
-		const expression = jsonata('response.hits[0].result.id');
-		const songId = expression.evaluate(JSON.parse(data));
-		const { lyrics } = await lyricist.song(songId, { fetchLyrics: true });
+	const query = new URLSearchParams([['q', 'Humble Kendric Lamar']]);
+	const data = (await client.get('/search', { query })).body;
+	const expression = jsonata('response.hits[0].result.id');
+	const songId = expression.evaluate(JSON.parse(data));
+	const { lyrics } = await lyricist.song(songId, { fetchLyrics: true });
 
-		callback(null, lyrics);
-	} catch (error) {
-		callback(error);
-	}
+	return lyrics;
 };
