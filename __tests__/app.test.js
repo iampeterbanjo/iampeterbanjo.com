f23a749b00e0b0723895cd671ade672a4214ed7b
iampeterbanjo <peterbanjo@gmail.com>
2019-03-18 05:38:39 +0000

    test korin was registered

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,35 +1,40 @@
-const Hapi = require('hapi')
-const { expect } = require('code')
-const { test, before, after } = (exports.lab = require('lab').script())
-const app = require('../server')
-const sinon = require('sinon')
+const Hapi = require('hapi');
+const { expect } = require('code');
+const { test, before, after } = (exports.lab = require('lab').script());
+const app = require('../server');
+const sinon = require('sinon');
 
-let server = Hapi.server(app.options)
+let server = Hapi.server(app.options);
 before(async () => {
-	sinon.spy(server, 'register')
-	require('../server/app')(server)
-})
+	sinon.spy(server, 'register');
+	require('../server/app')(server);
+});
 
 after(() => {
-	server.register.restore()
-})
+	server.register.restore();
+});
 
 test('good was registered first', () => {
-	const good = require('../server/good')
-	expect(server.register.firstCall.calledWith(good)).to.be.true()
-})
+	const good = require('../server/good');
+	expect(server.register.firstCall.calledWith(good)).to.be.true();
+});
 
 test('cqc was registered', () => {
-	const cqc = require('../server/cqc')
-	expect(server.register.calledWith(cqc)).to.be.true()
-})
+	const cqc = require('../server/cqc');
+	expect(server.register.calledWith(cqc)).to.be.true();
+});
 
 test('https-here was registered', () => {
-	const httpsHere = require('../server/https-here')
-	expect(server.register.calledWith(httpsHere)).to.be.true()
-})
+	const httpsHere = require('../server/https-here');
+	expect(server.register.calledWith(httpsHere)).to.be.true();
+});
 
 test('statics was registered', () => {
-	const statics = require('../server/statics')
-	expect(server.register.calledWith(statics)).to.be.true()
-})
+	const statics = require('../server/statics');
+	expect(server.register.calledWith(statics)).to.be.true();
+});
+
+test('korin was registered', () => {
+	const korin = require('../server/korin');
+	expect(server.register.calledWith(korin)).to.be.true();
+});
