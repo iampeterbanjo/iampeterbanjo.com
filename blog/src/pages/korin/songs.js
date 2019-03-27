27c0bf782701aeb799d2906c417164ae97bd4aa3
iampeterbanjo <peterbanjo@gmail.com>
2019-03-27 22:44:39 +0000

    style top 40 tracks from lastfm

--- blog/src/pages/korin/songs.js
+++ blog/src/pages/korin/songs.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import * as R from 'ramda';
 
 import BioMini from '../../components/bio-mini';
 import Layout from '../../components/layout';
@@ -8,22 +9,50 @@ export default props => {
 	const { pageContext } = props;
 	const { title, description, data = {} } = pageContext;
 
-	const { tracks = {} } = data;
-	const { songs = [] } = tracks;
-	console.log(tracks);
+	const tracks = R.pathOr([], ['tracks', 'track'], data);
+	const truncateText = text => {
+		const maxLength = 30;
+		if (text.length < maxLength) {
+			return text;
+		}
+		return `${text.slice(0, maxLength)}...`;
+	};
 
 	return (
 		<Layout>
 			<SEO title={title} description={description} />
 
-			<div className="cf mw9 center">
-				<h1 className="f2">Songs</h1>
-				<ul>
-					{songs.map(s => {
-						return <li>{s.name}</li>;
+			<article className="cf mw9 center">
+				<h1 className="f2">Top 40 tracks from LastFM</h1>
+
+				<ul className="cf pa0 list">
+					{tracks.map(track => {
+						const image = R.pathOr('', ['image', 3, '#text'], track);
+						const artist = R.path(['artist', 'name'], track);
+						const title = `${track.name} by ${artist}`;
+						const trackName = truncateText(track.name);
+
+						return (
+							<li className="fl w-50 w-25-m w-20-l">
+								<a
+									className="db mw5 black link dim"
+									title={title}
+									href={track.url}
+								>
+									<img className="db ba b--black-10" alt={title} src={image} />
+
+									<dl className="mt2 f6 lh-copy">
+										<dt className="clip">Title</dt>
+										<dd className="ml0 fw9">{trackName}</dd>
+										<dt className="clip">Artist</dt>
+										<dd className="ml0 gray">{artist}</dd>
+									</dl>
+								</a>
+							</li>
+						);
 					})}
 				</ul>
-			</div>
+			</article>
 
 			<BioMini />
 		</Layout>
