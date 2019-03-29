0be323fec8e358e81aeae8b9dcc784c0efaa05cf
iampeterbanjo <peterbanjo@gmail.com>
2019-03-29 09:17:48 +0000

    test db cache connection

--- server/__tests__/server.test.js
+++ server/__tests__/server.test.js
@@ -1,14 +1,25 @@
 const Hapi = require('hapi');
 const { expect } = require('code');
-const { test, suite } = (exports.lab = require('lab').script());
-
+const { test, suite, before } = (exports.lab = require('lab').script());
+const R = require('ramda');
 const { options } = require('../index');
 const Server = () => Hapi.server(options);
 
 suite('cache', () => {
-	test('mongodb-cache is provisioned', async ({ context }) => {
-		const [provisioned] = Server().settings.cache;
+	before(({ context }) => {
+		const path = ['settings', 'cache', 0];
+		context.provisioned = R.path(path, Server());
+	});
 
-		expect(provisioned.name).to.equal(`mongodb-cache`);
+	test('mongodb-cache is provisioned', async ({ context }) => {
+		const { name } = context.provisioned;
+
+		expect(name).to.equal(`mongodb-cache`);
+	});
+
+	test(`mongodb-cache connection`, ({ context }) => {
+		const { uri } = R.path(['provider', 'options'], context.provisioned);
+
+		expect(uri).to.equal(process.env.MONGODB_URI);
 	});
 });
--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const path = require('path');
+const { PORT, MONGODB_URI } = process.env;
 
 module.exports = {
 	rootPath: path.join(__dirname, '../blog/public/'),
 	options: {
 		host: '0.0.0.0',
-		port: Number(process.env.PORT || 8080),
+		port: Number(PORT || 8080),
 		routes: {
 			files: {
 				relativeTo: __dirname,
@@ -16,7 +17,7 @@ module.exports = {
 				provider: {
 					constructor: require('catbox-mongodb'),
 					options: {
-						uri: 'your-mongodb-uri',
+						uri: MONGODB_URI,
 						partition: 'cache',
 					},
 				},
