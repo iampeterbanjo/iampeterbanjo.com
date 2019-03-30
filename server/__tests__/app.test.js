290b7f26d4a9605d41255ea75f70133427dc6553
iampeterbanjo <peterbanjo@gmail.com>
2019-03-30 08:46:31 +0000

    fix verbose import

--- server/__tests__/app.test.js
+++ server/__tests__/app.test.js
@@ -1,13 +1,13 @@
 const Hapi = require('hapi');
 const { expect } = require('code');
 const { test, before, after } = (exports.lab = require('lab').script());
-const { options } = require('../../server');
+const { options } = require('..');
 const sinon = require('sinon');
 
 let server = Hapi.server(options);
 before(async () => {
 	sinon.spy(server, 'register');
-	require('../../server/app')(server);
+	require('../app')(server);
 });
 
 after(() => {
@@ -15,26 +15,26 @@ after(() => {
 });
 
 test('good was registered first', () => {
-	const good = require('../../server/good');
+	const good = require('../good');
 	expect(server.register.firstCall.calledWith(good)).to.be.true();
 });
 
 test('cqc was registered', () => {
-	const cqc = require('../../server/cqc');
+	const cqc = require('../cqc');
 	expect(server.register.calledWith(cqc)).to.be.true();
 });
 
 test('https-here was registered', () => {
-	const httpsHere = require('../../server/https-here');
+	const httpsHere = require('../https-here');
 	expect(server.register.calledWith(httpsHere)).to.be.true();
 });
 
 test('statics was registered', () => {
-	const statics = require('../../server/statics');
+	const statics = require('../statics');
 	expect(server.register.calledWith(statics)).to.be.true();
 });
 
 test('korin was registered', () => {
-	const korin = require('../../server/korin');
+	const korin = require('../korin');
 	expect(server.register.calledWith(korin)).to.be.true();
 });
