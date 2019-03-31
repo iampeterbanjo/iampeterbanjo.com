1ffee60c35eb93dcd506124de24a981467b7422d
iampeterbanjo <peterbanjo@gmail.com>
2019-03-31 19:29:07 +0100

    change favicon

--- blog/plugins/gatsby-plugin-manifest.js
+++ blog/plugins/gatsby-plugin-manifest.js
@@ -7,6 +7,6 @@ module.exports = {
 		background_color: `#ffffff`,
 		theme_color: `#663399`,
 		display: `minimal-ui`,
-		icon: `content/assets/gatsby-icon.png`,
+		icon: `content/assets/iampeterbanjo-w400.jpg`,
 	},
-}
+};
