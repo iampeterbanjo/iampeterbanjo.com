ac4cf267c839c1857153c40d2c42c7f42ebe6bbb
iampeterbanjo <peterbanjo@gmail.com>
2019-04-13 21:48:25 +0100

    fix e2e tests

--- packages/e2e/cypress/integration/blog-details.spec.js
+++ packages/e2e/cypress/integration/blog-details.spec.js
@@ -1,4 +1,7 @@
 const BASE_URL = 'http://localhost:8080';
+const { clearServiceWorkers } = require('./helpers');
+
+beforeEach(clearServiceWorkers);
 
 describe('Blog', () => {
 	beforeEach(() => {
--- packages/e2e/cypress/integration/layout.spec.js
+++ packages/e2e/cypress/integration/layout.spec.js
@@ -1,5 +1,8 @@
 const BASE_URL = 'http://localhost:8080';
 const slugs = require('../../../blog/pages/slugs');
+const { clearServiceWorkers } = require('./helpers');
+
+beforeEach(clearServiceWorkers);
 
 describe('layout', () => {
 	beforeEach(() => {
