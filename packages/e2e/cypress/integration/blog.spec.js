7490e19ca4ef0e4c1e7af4650964248b45c09463
iampeterbanjo <peterbanjo@gmail.com>
2019-04-30 06:05:31 +0100

    test for read more links

--- packages/e2e/cypress/integration/blog.spec.js
+++ packages/e2e/cypress/integration/blog.spec.js
@@ -22,4 +22,13 @@ describe('Blog', () => {
 	it('should have a mini-bio', () => {
 		cy.get('article:last-child').should('contain', 'Written by Peter Banjo');
 	});
+
+	it('should have "Read more" links', () => {
+		cy.get('.post-excerpt a[href]').then($readMoreLinks => {
+			expect($readMoreLinks.length).to.be.greaterThan(0);
+			$readMoreLinks.each((index, $link) => {
+				expect($link.textContent).to.eq('Read more');
+			});
+		});
+	});
 });
