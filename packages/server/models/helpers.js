27549a14a4eead14de8edd909625af167b1c78ab
iampeterbanjo <peterbanjo@gmail.com>
2019-05-15 07:12:35 +0100

    add model connex helper

---
+++ packages/server/models/helpers.js
@@ -0,0 +1,22 @@
+const Bluebird = require('bluebird');
+const { vars } = require('../utils');
+
+const { MONGODB_ADDON_DB, MONGODB_ADDON_URI } = vars;
+
+const connex = {};
+/**
+ * Get database name
+ * @param {boolean} [isTest]
+ * @return {string} database name
+ */
+connex.dbName = isTest => (isTest ? 'test' : MONGODB_ADDON_DB);
+connex.uri = MONGODB_ADDON_URI;
+connex.options = {
+	useNewUrlParser: true,
+	dbName: connex.dbName(process.env.NODE_ENV === 'test'),
+	promiseLibrary: Bluebird,
+};
+
+module.exports = {
+	connex,
+};
