ad34ce0d389c255ab1020ad89f0496bb2b823fea
iampeterbanjo <peterbanjo@gmail.com>
2019-08-29 06:56:20 +0100

    fix https here tests

--- packages/server/src/https-here/https-here.test.ts
+++ packages/server/src/https-here/https-here.test.ts
@@ -1,86 +1,90 @@
-// import Hapi from '@hapi/hapi';
-// import { expect } from '@hapi/code';
-// import Lab from '@hapi/lab';
-// import plugin from '../https-here';
+import Hapi from '@hapi/hapi';
+import plugin from '../https-here';
 
-// export const lab = Lab.script();
+import { makeBdd } from '../../factory';
+const { Given, And, When } = makeBdd({ describe, it });
 
-// const { test, suite } = lab;
+const Server = (options?: Object) => {
+	const server = Hapi.Server();
+	server.register({ plugin, options });
+	server.route({
+		method: 'GET',
+		path: '/',
+		handler(request, h) {
+			return h.response('Hello!');
+		},
+	});
+	return server;
+};
 
-// const Server = (options?: Object) => {
-// 	const server = Hapi.Server();
-// 	server.register({ plugin, options });
-// 	server.route({
-// 		method: 'GET',
-// 		path: '/',
-// 		handler(request, h) {
-// 			return h.response('Hello!');
-// 		},
-// 	});
-// 	return server;
-// };
+Given('https-here:', () => {
+	When('a proxied http request is made, its redirected to https', async () => {
+		const res = await Server().inject({
+			method: 'GET',
+			url: '/',
+			headers: {
+				host: 'host',
+				'x-forwarded-proto': 'http',
+			},
+		});
 
-// Given('https-here:', () => {
-// 	When('proxied http requests are redirected to https', async () => {
-// 		const res = await Server().inject({
-// 			method: 'GET',
-// 			url: '/',
-// 			headers: {
-// 				host: 'host',
-// 				'x-forwarded-proto': 'http',
-// 			},
-// 		});
-// 		expect(res.statusCode).toEqual(301);
-// 		expect(res.headers.location).toEqual('https://host/');
-// 	});
+		expect(res.statusCode).toEqual(301);
+		expect(res.headers.location).toEqual('https://host/');
+	});
 
-// 	When('un-proxied http requests are redirected to https', async () => {
-// 		const res = await Server({ proxy: false }).inject({
-// 			url: '/',
-// 			headers: {
-// 				host: 'host',
-// 			},
-// 		});
-// 		expect(res.statusCode).toEqual(301);
-// 		expect(res.headers.location).toEqual('https://host/');
-// 	});
+	When(
+		'an unproxied http request is made, its redirected to https',
+		async () => {
+			const res = await Server({ proxy: false }).inject({
+				url: '/',
+				headers: {
+					host: 'host',
+				},
+			});
 
-// 	When('query string', async () => {
-// 		const res = await Server().inject({
-// 			url: '/?test=test&test2=test2',
-// 			headers: {
-// 				host: 'host',
-// 				'x-forwarded-proto': 'http',
-// 			},
-// 		});
-// 		expect(res.statusCode).toEqual(301);
-// 		expect(res.headers.location).toEqual(
-// 			'https://host/?test=test&test2=test2',
-// 		);
-// 	});
+			expect(res.statusCode).toEqual(301);
+			expect(res.headers.location).toEqual('https://host/');
+		},
+	);
 
-// 	When('ignores unmatched', async () => {
-// 		const res = await Server().inject({
-// 			url: '/',
-// 			headers: {
-// 				host: 'host',
-// 				'x-forwarded-proto': 'https',
-// 			},
-// 		});
-// 		expect(res.statusCode).toEqual(200);
-// 		expect(res.result).toEqual('Hello!');
-// 	});
+	When('a request is redirected the query string is preserved', async () => {
+		const res = await Server().inject({
+			url: '/?test=test&test2=test2',
+			headers: {
+				host: 'host',
+				'x-forwarded-proto': 'http',
+			},
+		});
 
-// 	When('x-forward-host support', async () => {
-// 		const res = await Server().inject({
-// 			url: '/',
-// 			headers: {
-// 				host: 'host',
-// 				'x-forwarded-proto': 'http',
-// 				'x-forwarded-host': 'host2',
-// 			},
-// 		});
-// 		expect(res.statusCode).toEqual(301);
-// 		expect(res.headers.location).toEqual('https://host2/');
-// 	});
-// });
+		expect(res.statusCode).toEqual(301);
+		expect(res.headers.location).toEqual('https://host/?test=test&test2=test2');
+	});
+
+	When('a https request is made it is not redirected', async () => {
+		const res = await Server().inject({
+			url: '/',
+			headers: {
+				host: 'host',
+				'x-forwarded-proto': 'https',
+			},
+		});
+		expect(res.statusCode).toEqual(200);
+		expect(res.result).toEqual('Hello!');
+	});
+
+	When(
+		'a request is redirected the x-forward-host support is used',
+		async () => {
+			const res = await Server().inject({
+				url: '/',
+				headers: {
+					host: 'host',
+					'x-forwarded-proto': 'http',
+					'x-forwarded-host': 'host2',
+				},
+			});
+			expect(res.statusCode).toEqual(301);
+			expect(res.headers.location).toEqual('https://host2/');
+		},
+	);
+});
