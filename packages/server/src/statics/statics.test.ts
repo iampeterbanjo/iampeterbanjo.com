ee0d9e744a0e68d76e2d2ad3f3f2a2398b63f997
iampeterbanjo <peterbanjo@gmail.com>
2019-08-30 21:17:30 +0100

    fix statics test

--- packages/server/src/statics/statics.test.ts
+++ packages/server/src/statics/statics.test.ts
@@ -1,36 +1,34 @@
-// import Hapi from '@hapi/hapi';
-// import { expect } from '@hapi/code';
-// import Lab from '@hapi/lab';
+import Hapi from '@hapi/hapi';
 
-// import Inert from '@hapi/inert';
-// import path from 'path';
-// import plugin from '../statics/plugin';
+import Inert from '@hapi/inert';
+import path from 'path';
 
-// export const lab = Lab.script();
-// const { test, beforeEach, suite } = lab;
+import plugin from './plugin';
+import { makeBdd } from '../../factory';
 
-// const cssPath = '../css';
-// const jsPath = '../js';
-// const imagePath = '../images';
-// const rootPath = path.join(__dirname, './fixtures');
+const { Given, When } = makeBdd({ describe, it });
+const cssPath = '../../../css';
+const jsPath = '../../../js';
+const imagePath = '../../../images';
+const rootPath = path.join(__dirname, '../../fixtures');
 
-// let server;
-// beforeEach(async () => {
-// 	server = Hapi.server();
-// 	await server.register(Inert);
-// });
+let server;
+beforeEach(async () => {
+	server = Hapi.server();
+	await server.register(Inert);
+});
 
-// Given('statics', () => {
-// 	When('/ rootPath is served', async () => {
-// 		await server.register({
-// 			plugin,
-// 			options: { rootPath, cssPath, jsPath, imagePath },
-// 		});
-// 		const res = await server.inject({
-// 			method: 'GET',
-// 			url: '/',
-// 		});
+Given('statics', () => {
+	When('/ rootPath is served statusCode is 200', async () => {
+		await server.register({
+			plugin,
+			options: { rootPath, cssPath, jsPath, imagePath },
+		});
+		const res = await server.inject({
+			method: 'GET',
+			url: '/',
+		});
 
-// 		expect(res.statusCode).toEqual(200);
-// 	});
-// });
+		expect(res.statusCode).toEqual(200);
+	});
+});
