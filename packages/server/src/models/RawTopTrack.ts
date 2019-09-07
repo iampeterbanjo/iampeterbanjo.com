76ac17145ec3ce9b1dea11a6bbf58fdb1627e858
iampeterbanjo <peterbanjo@gmail.com>
2019-09-07 11:30:39 +0100

    create RawTrack model using ts interface

--- packages/server/src/models/RawTopTrack.ts
+++ packages/server/src/models/RawTopTrack.ts
@@ -1,27 +1,65 @@
-import mongoose from 'mongoose';
+import { Schema, model, Document, Model } from 'mongoose';
 
-const RawTopTrackSchema = new mongoose.Schema({
-	name: String,
-	duration: String,
-	playcount: String,
-	subscribers: String,
-	mbid: String,
-	url: String,
+declare interface IRawTopTrack extends Document {
+	name: string;
+	duration: string;
+	playcount: string;
+	subscribers: string;
+	mbid: string;
+	url: string;
 	streamable: {
-		'#text': String,
-		fulltrack: String,
-	},
+		'#text': string;
+		fulltrack: string;
+	};
 	artist: {
-		name: String,
-		mbid: String,
-		url: String,
-	},
+		name: string;
+		mbid: string;
+		url: string;
+	};
 	image: [
 		{
-			'#text': String,
-			size: String,
+			'#text': string;
+			size: string;
 		},
-	],
-});
+	];
+}
 
-export default mongoose.model('RawTopTrack', RawTopTrackSchema);
+export interface RawTopTrackModel extends Model<IRawTopTrack> {}
+
+export class RawTopTrack {
+	private _model: Model<IRawTopTrack>;
+
+	constructor() {
+		const schema = new Schema({
+			name: String,
+			duration: String,
+			playcount: String,
+			subscribers: String,
+			mbid: String,
+			url: String,
+			streamable: {
+				'#text': String,
+				fulltrack: String,
+			},
+			artist: {
+				name: String,
+				mbid: String,
+				url: String,
+			},
+			image: [
+				{
+					'#text': String,
+					size: String,
+				},
+			],
+		});
+
+		this._model = model<IRawTopTrack>('RawTopTrack', schema);
+	}
+
+	public get model(): Model<IRawTopTrack> {
+		return this._model;
+	}
+}
+
+export default new RawTopTrack().model;
