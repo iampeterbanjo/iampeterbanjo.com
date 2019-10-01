01723bd230ae5378e18f0287248e7981b846fda9
iampeterbanjo <peterbanjo@gmail.com>
2019-10-01 21:23:32 +0100

    add security plugin

---
+++ packages/server/src/security/index.ts
@@ -0,0 +1,14 @@
+import Hodor from 'hapi-hodor';
+import utils from '../utils';
+
+const { vars } = utils;
+
+export default {
+	plugin: Hodor,
+	options: {
+		sessionSecretKey: vars.SESSION_SECRET_KEY,
+		auth0Domain: vars.AUTH0_DOMAIN,
+		auth0PublicKey: vars.AUTH0_PUBLIC_KEY,
+		auth0SecretKey: vars.AUTH0_SECRET_KEY,
+	},
+};
