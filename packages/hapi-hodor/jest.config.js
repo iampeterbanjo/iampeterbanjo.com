694818db54a8d0da268ea21fdfabce97c286e228
iampeterbanjo <peterbanjo@gmail.com>
2019-10-01 07:03:15 +0100

    hapi-hodor submodule

--- packages/hapi-hodor/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-	preset: 'ts-jest',
-	testEnvironment: 'node',
-	collectCoverage: true,
-	coverageReporters: ['json-summary', 'text-summary'],
-	collectCoverageFrom: ['src/**/*.{ts,tsx}', '!**/node_modules/**'],
-	notify: true,
-	notifyMode: 'failure-change',
-};
