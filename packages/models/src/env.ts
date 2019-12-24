4b0e3294645e650b0f05ab12ef844706ea481012
iampeterbanjo <peterbanjo@gmail.com>
2019-12-24 16:27:04 +0000

    remove models - prep submodules

--- packages/models/src/env.ts
+++
@@ -1,2 +0,0 @@
-export const MONGODB_ADDON_URI = process.env.MONGODB_ADDON_URI || '';
-export const MONGODB_ADDON_DB = process.env.MONGODB_ADDON_DB || '';
--- packages/models/src/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './database';
-export * from './config';
-export * from './ChartTrack';
-export * from './ChartRawTrack';
-export * from './TrackProfile';
-export * from './types';
