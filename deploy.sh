f3d033b9ea05344b30bcf7570a7389a5d61a8596
iampeterbanjo <gitblame@iampeterbanjo.com>
2020-03-08 08:50:56 +0000

    update publish config

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 # deploy project, make sure badges are updated
+npx lerna publish
 npm run lint && npm run test
 git add packages/server/badges/
 git commit -m 'update badges'
